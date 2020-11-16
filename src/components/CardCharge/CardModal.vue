a8ca18d7cb9da18ea0c86a9022f4fdf554611413
josuwon <vv_vv3371@naver.com>
2020-11-16 10:49:02 +0900

    card-pay

---
+++ src/components/CardCharge/CardModal.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-dialog 
+    v-model="visible" 
+    width="800px" 
+    transition="slide-y-transition"
+    overlay-opacity="0.8"
+    overlay-color="#000"
+  >
+    <div class="paymentModal">
+      <div class="paymentImg">
+        <img src="@/assets/img/paymentCard.png">
+      </div>
+      <div class="priceInfo">
+        <dl>
+          <dt>선택상품</dt>
+          <dd>2,000포인트 충전</dd>
+        </dl>
+        <dl>
+          <dt>결제금액</dt>
+          <dd>2,000원</dd>
+        </dl>
+        <dl>
+          <dt>나의포인트</dt>
+          <dd>2,400</dd>
+        </dl>
+        <dl>
+          <dt>추가적립금</dt>
+          <dd>500</dd>
+        </dl>
+        <dl class="lastPoint"> 
+          <dt>최종포인트</dt>
+          <dd>2,900</dd>
+        </dl>
+      </div>
+      <div class="guide">
+        <strong>IC카드를 투입구에 넣어주세요</strong>
+        <p>삑소리 이후에 카드를 투입하시고, <br>결제가 끝날때까지 카드를 빼지 마세요</p>
+      </div>
+    </div>
+  </v-dialog>
+</template>
+
+<script>
+import ModalMixin from '@/mixins/modal';
+export default {
+  name: 'CardModal',
+  mixins: [ModalMixin],
+};
+</script>
+
+<style lang="scss" scoped>
+.paymentModal{
+  width:100%;
+  background:#fff;
+  padding:60px;
+  border-radius:10px;
+  position:relative;
+  .paymentImg{
+    background:#e2e2e2;
+    text-align:center;
+    padding:40px;
+    border-radius:10px;
+  }
+  .priceInfo{
+    margin-top:60px;
+    dl{
+      display:flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom:20px;
+      dt{font-size:32px;font-weight:500;color:#888}
+      dd{font-size:32px;font-weight:700}
+    }
+    dl.nowPrice{
+      dd{color:#ee2073}
+    }
+    dl:last-child{margin-bottom:0px;}
+    dl.lastPoint{color:#ee2073}
+  }
+  .guide{
+    text-align:center;
+    margin-top:40px;
+    border-top:2px solid #e2e2e2;
+    padding-top:40px;
+    strong{font-size:36px;letter-spacing:-1px;font-weight:500;margin-bottom:20px;display:block;}
+    p{font-size:28px;color:#ee2073;word-break:keep-all}
+  }
+  .counter{
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    margin-top:40px;
+    span{
+      display:inline-block;
+      width:120px;height:120px;
+      border-radius:60px;
+      display:flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      background:#f2f2f2;
+      font-size:42px;
+      font-weight:600;
+    }
+  }
+  .closeBtn{
+    position:absolute;
+    top:0;
+    left:50%;
+    width:120px;
+    height:120px;
+    border-radius:50%;
+    margin-left:-60px;
+    margin-top:-60px;
+    background:#0085DE;
+    color:#fff;
+    border:0px;
+    .v-icon{
+      font-size:52px
+    }
+  }
+}
+</style>
---
+++ src/components/modal/Terms.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-dialog
+    v-model="visible"
+    class="dialog"
+    width="960px"
+  >
+    <div class="terms">
+      <div class="termsHead">
+        {{serviceTerms.title}}
+      </div>
+
+      <div class="termsBody" v-html="serviceTerms.body" @click="touchBody">
+
+      </div>
+
+      <div class="termsBottom">
+        <v-btn outlined @click="open">닫기</v-btn>
+      </div>
+    </div>
+  </v-dialog>
+</template>
+
+<script>
+import { info, user as userInfo } from '@/assets/docs/terms';
+export default {
+  data(){
+    return{
+      visible:false,
+      serviceTerms: {
+        title: '개인정보 취급방침',
+        body: userInfo.replace(/\n/g, '<br />')
+      },      
+      touchCount: 0,
+    }
+  },
+  watch: {
+    visible(newValue) {
+      if(!newValue) this.touchCount = 0;
+    },
+    touchCount(newValue) {
+      if(newValue === 15) {
+        this.$router.push({ name: 'AccountLogin' });
+      }
+    },
+  },
+  methods:{
+    open(type){
+      if(type === 'user') {
+        this.serviceTerms.title = '개인정보 취급방침';
+        this.serviceTerms.body = userInfo.replace(/\n/g, '<br />');
+        this.visible = !this.visible;
+      } else if(type === 'info') {
+        this.serviceTerms.title = '키오스크 이용약관';
+        this.serviceTerms.body = info.replace(/\n/g, '<br />');
+        this.visible = !this.visible;
+      }
+    },
+    touchBody() {
+      this.touchCount++;
+    },
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.terms{
+  
+  background:#fff;
+  border-radius:20px;
+  overflow:hidden;
+  
+  .termsHead{
+    display:flex;
+    align-items: center;
+    font-size:36px;
+    font-weight:500;
+    height:100px;
+    padding:0 40px;
+    border-bottom:2px solid #e2e2e2
+  }
+  .termsBody{
+    height:1200px;
+    padding:40px;
+    overflow-y:auto;
+    font-size:24px;
+    iframe{
+      border:0px;
+      font-size:24px;
+    }
+  }
+  .termsBottom{
+    display:flex;
+    justify-content: center;
+    align-items:center;
+    height:150px;
+    border-top:2px solid #e2e2e2;
+    .v-btn{
+      font-size:32px;
+      width:300px;
+      height:80px;
+      background:#292929;
+      color:#fff;
+      font-weight:500;
+      border-radius:40px;
+    }
+  }
+}
+</style>
