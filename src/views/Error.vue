158f82b1fade298aa36ca72be843853f76cc05aa
josuwon <vv_vv3371@naver.com>
2021-06-14 14:28:10 +0900

    에러페이지에서 왼쪽상단 구석 15회 터치시 앱종료 & 키오스크PC재부팅 버튼 생성

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="examine">
+    <div class="leftTop" v-touchEvent:tap="() => exitCount++"></div>
+    <div class="rightTop" v-touchEvent:tap="rebootPC">키오스크 PC 재부팅</div>
     <v-icon>mdi-lightbulb</v-icon>
     <dl>
       <dt>{{ title }}</dt>
       <dd v-if="message">
         {{ message }}
       </dd>
+      <dd>고객센터 : {{ companyTel }}</dd>
     </dl>
   </div>
 </template>
 
 <script>
+import { ipcRenderer } from 'electron';
+import { mapState } from 'vuex';
+import { reboot } from 'electron-shutdown-command';
 export default {
   name: 'ErrorPage',
   props: {
@@ -20,11 +26,52 @@ export default {
     },
     message: String,
   },
+  data() {
+    return {
+      exitCount: 0,
+    };
+  },
+  computed: {
+    ...mapState({
+      companyTel: state => state.company.phone,
+    }),
+  },
+  watch: {
+    exitCount(newValue) {
+      if (newValue >= 15) {
+        ipcRenderer.invoke('exit-app');
+      }
+    },
+  },
+  methods: {
+    rebootPC() {
+      reboot();
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .examine {
+  .leftTop {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100px;
+    height: 100px;
+  }
+  .rightTop {
+    position: absolute;
+    right: 0;
+    top: 0;
+    margin-right: 20px;
+    margin-top: 20px;
+    color: #292929;
+    background: #fff;
+    padding: 5px 20px;
+    border-radius: 10px;
+  }
+
   display: flex;
   flex-direction: column;
   justify-content: center;
